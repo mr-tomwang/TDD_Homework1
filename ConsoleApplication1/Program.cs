d5370277ad21bec4b290eda4604f50dd5119ead6
Tom Wang <tom_wang@trend.com.tw>
2016-01-05 00:56:58 +0800

    TDD Homework 1

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+
+namespace ConsoleApplication1
+{
+    public class Table {
+
+        private List<int> _Id;
+        private List<int> _Cost;
+        private List<int> _Revenue;
+        private List<int> _SellPrice;
+
+        public void SetIds(List<int> id)
+        {
+            _Id = id;
+        }
+        public void SetCosts(List<int> cost)
+        {
+            _Cost = cost;
+        }
+        public void SetRevenues(List<int> revenue)
+        {
+            _Revenue = revenue;
+        }
+        public void SetSellPrices(List<int> price)
+        {
+            _SellPrice = price;
+        }
+        public List<int> CreateGroups(string colName, int groupSize)
+        {
+            List<int> output = new List<int>();
+            List<int> source = null;
+            if (colName == "Revenue")
+                source = _Revenue;
+            else if (colName == "Cost")
+                source = _Cost;
+            if (source == null)
+                return output;
+
+            int sum = 0;
+            int iterate = 0;
+            for (int i = 0; i < source.Count; i++)
+            {
+                sum += source[i];
+                iterate++;
+                if (iterate == groupSize)
+                {
+                    output.Add(sum);
+                    sum = 0;
+                    iterate = 0;
+                }
+            }
+            // check remaining one
+            if (sum != 0)
+            {
+                output.Add(sum);
+            }
+
+            return output;
+        }
+
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Table table = new Table();
+            table.SetIds        (new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
+            table.SetCosts      (new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
+            table.SetRevenues   (new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
+            table.SetSellPrices (new List<int> { 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 });
+
+            List<int> costGroup = table.CreateGroups("Cost", 3);
+            List<int> revenueGroup = table.CreateGroups("Revenue", 4);
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ ConsoleApplication1Tests/TableTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class TableTests
+    {
+        [TestMethod()]
+        public void CreateGroupsTest_UnknownName_AnySize()
+        {
+            // arrange
+            Table table = new Table();
+            table.SetSellPrices(new List<int> { 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 });
+            List<int> expected = new List<int>();
+            // act
+            List<int> actual = table.CreateGroups("SellPrice", 10);
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CreateGroupsTest_Cost_3()
+        {
+            // arrange
+            Table table = new Table();
+            table.SetCosts(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 });
+            List<int> expected = new List<int> { 6, 15, 24, 21 };
+            // act
+            List<int> actual = table.CreateGroups("Cost", 3);
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void CreateGroupsTest_Revenue_4()
+        {
+            // arrange
+            Table table = new Table();
+            table.SetRevenues(new List<int> { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 });
+            List<int> expected = new List<int> { 50, 66, 60 };
+            // act
+            List<int> actual = table.CreateGroups("Revenue", 4);
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
